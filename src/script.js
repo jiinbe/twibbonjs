af69442942d14e08fcdb372b44cc366c90e072cc
Ferry Ayunda <165256908+lonteluh@users.noreply.github.com>
2024-06-24 10:42:37 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -52,9 +52,11 @@ if(this.files && this.files[0]) {
 statusElement.textContent = config.messages.uploading;
 			
 let uploadedImage = document.createElement('img');
+let heightRatio = 1.5;
+uploadedImage.height = canvas.width * heightRatio;
 uploadedImage.src = URL.createObjectURL(this.files[0]);
-uploadedImage.setAttribute("width", "1024");
-uploadedImage.setAttribute("height", "1024");
+// uploadedImage.setAttribute("width", "1024");
+// uploadedImage.setAttribute("height", "1024");
 			
 uploadedImage.addEventListener('load', function() {
 statusElement.textContent = config.messages.status.processing;
