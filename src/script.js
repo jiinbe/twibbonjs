e09588f16fb8925104fef8afeedf975be5b8b088
Ferry Ayunda <165256908+lonteluh@users.noreply.github.com>
2024-06-24 22:28:50 +0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -30,7 +30,7 @@ mainElement.appendChild(statusElement);
 let fileUploadElement = document.createElement('input');
 fileUploadElement.type = 'file';
 fileUploadElement.accept = "image/*";
-// fileUploadElement.classList.add("file");
+fileUploadElement.classList.add("image-upload");
 mainElement.appendChild(fileUploadElement);
 	
 let overlayImageElement = new Image();
